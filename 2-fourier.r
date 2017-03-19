65288633b898e833474906bc81ecd51f139d949e
Felipe Duarte <felipelageduarte@gmail.com>
2017-03-19 20:11:06 -0300

    incluindo validaçao cruzada out-of-time

---
+++ 2-fourier.r
@@ -0,0 +1,46 @@
+
+require(TSDecomposition)
+require(foreach)
+require(parallel)
+require(doMC)
+require(Rssa)
+require(FNN)
+
+source('utils.r')
+
+cores = detectCores(all.tests = FALSE, logical = TRUE)
+
+dataFolder = 'data'
+modelFolder  = 'model'
+resultFolder = 'testResult'
+
+seriesList = loadSeriesFile(dataFolder)
+
+fourierDec <- function(series, par){
+    freq.cutoff = unlist(par[1])
+
+    coeffs = fft(series)
+    mags   = coeffs[1:(length(coeffs)/2)]
+    mags   = 1+sqrt(Re(mags)^2+Im(mags)^2)
+    o.idx  = order(mags, decreasing = T)
+    idx    = (1:length(mags))[-o.idx[1:freq.cutoff]]
+        
+    coeffs[idx] = complex(real=0, imaginary=0)
+    coeffs[length(coeffs) - idx + 1] = complex(real=0, imaginary=0)
+    det = Re(fft(coeffs, inverse=T)) / length(series)
+    
+    return(det)
+}
+
+#all combination of possible parameters for Fourier Algorithm
+hyperparameters = expand.grid(
+  cutoff = 1:500,
+  stringsAsFactors = FALSE
+)
+
+resultTable = gridSearch(fourierDec, hyperparameters, seriesList, modelFolder, 'fourier', cores, TRUE)
+
+write.csv(resultTable, file=paste(resultFolder,'/fourier.csv', sep=''), row.names=FALSE)
+
+
+
--- utils.r
+++ utils.r
@@ -1,149 +1,116 @@
 md.dist <- function(d1, d2){
-	if(ncol(d1) != ncol(d2)) stop("Arrays must have the same dimensions")
-	if(nrow(d1) != nrow(d2)) stop("Arrays must have the same dimensions")
-
-	d = d1 - d2
-	sqrt(diag(d%*%t(d)))
+    d = d1 - d2
+    mean(sqrt(diag(d%*%t(d))))
 }
 
-
-mddl <-function(det.original, det.result){
-	s1 = det.original
-	s2 = det.result
-
-	if(length(s1) > length(s2)){
-		s.aux = s1
-		s1 = s2
-		s2 = s.aux
-	}
-
-	n.elem = length(s1)
-	delta = length(s2) - length(s1)
-
-	mddl = Inf
-	for(i in 0:delta){
-		d1     = s1
-		d2     = s2[(1+i):(n.elem+i)]
-		mddl = min(mddl, TSDecomposition::mddl(d1, d2, plot=FALSE))
-	}
-	return(mddl)
+mddl <-function(obs, pred){
+    TSDecomposition::mddl(obs, pred, plot=FALSE)
 }
 
+mda <- function(obs, pred, m, d){
+    at1 = embedd(obs,  m=m, d=d)
+    at2 = embedd(pred, m=m, d=d)
 
-mda <- function(det.original, det.result, m, d){
-	at1 = embedd(det.original, m=m, d=d)
-	at2 = embedd(det.result, m=m, d=d)
+    n.elem = nrow(at1)
+    delta = nrow(at2) - nrow(at1)
 
-	if(nrow(at1) > nrow(at2)){
-		at.aux = at1
-		at1 = at2
-		at2 = at.aux
-	}
-
-	n.elem = nrow(at1)
-	delta = nrow(at2) - nrow(at1)
-
-	mda = Inf
-	for(i in 0:delta){
-		d1     = at1
-		d2     = at2[(1+i):(n.elem+i),]
-		p.dist = md.dist(d1,d2)
-		mda    = min(mda, p.dist)
-	}
-
-	return(mda)
+    if(delta == 0){
+        return(md.dist(at1, at2))
+    } else {
+        return(min(lapply(0:delta, function(x) md.dist(at1, at2[(1+x):(n.elem+x),]))))
+    }
+}
+        
+loadSeriesFile <- function(seriesFolder){
+    #select all series file in the data folder
+    seriesFile = list.files(path = seriesFolder, full.names = TRUE)
+    seriesList = list()
+    for(i in 1:length(seriesFile)){
+        load(seriesFile[i])
+        seriesList[[i]] = seriesObj
+    }
+    seriesList
 }
 
-gridSearch <- function(F, params, seriesFile, tech.name, cores = 1){
-
-	registerDoMC(cores)
-
-	resultTable = foreach(i=1:length(seriesFile), .combine='rbind') %dopar% {
-		s = seriesFile[i]
-		resultTable.aux = matrix(ncol=6, nrow = 0)
-
-		#load series object from RData file
-		load(s)
-
-		mddl.results = c()
-		mda.results = c()
-
-		for(p in 1:nrow(params)){
-
-			result = c()
-
-			#apply model on the time series
-			tryCatch({
-				result = F(seriesObj, params[p,])
-			}, warning = function(w){
-				print(w)
-			}, error = function(e){
-				print(e)
-			}, finally = {
-			})
-
-			#---------------------------------------------------------------------------
-			# This sequence of if-else is necessary because:
-			# 1) Non all of parameter combination is valid and in this case the function 
-			#    F will throw exception and return NULL as result. Instead of map all 
-			#    valid combination we prefer to verify after code execution to keep it 
-			#    more readable.
-			# 2) We embedded the evaluation inside ForceDec execution in attempt to make 
-			#    test more fast. In this situation the result will be a list with MDA 
-			#    and MDDL values. 
-			# 3) In all other non exception scenarios will return the deterministic 
-			#    component and all evaluation must be done.
-			#---------------------------------------------------------------------------
-			if(is.null(result)){
-				next
-			} else if(is.list(result)){ 
-				mddl.results = c(mddl.results, result$mddl)
-				mda.results  = c(mda.results,  result$mda)
-
-				for(k in 1:length(result$mddl)){
-					par    = params[p,]
-					par[2] = k
-
-					resultTable.aux  = rbind(resultTable.aux, c( ((p-1)*length(result$mddl)) + k, 
-																tech.name, 
-																paste(par, collapse=","), 
-																s, 
-																result$mddl[k], 
-																result$mda[k]))
-				} 
-			} else{
-				mddl.r = mddl(seriesObj$det.series, result)
-				mda.r  = mda (seriesObj$det.series, result, seriesObj$det.embDim, seriesObj$det.sepDim)
-				mddl.results = c(mddl.results, mddl.r)
-				mda.results  = c(mda.results,  mda.r)
-				resultTable.aux  = rbind(resultTable.aux, c(p, tech.name, paste(params[p,],collapse=","), s, mddl.r, mda.r))
-			}
-
-			print(paste("Done:",i,'\n'))
-		}
-
-		#normalize results and calculate L2-norm
-		l2n.results      = sqrt((mddl.results/max(mddl.results))^2 + (mda.results/max(mda.results))^2)
-		best.result.idx  = which.min(l2n.results)
-		best.result.idx  = ifelse(length(best.result.idx) == 0, 1, best.result.idx)
-
-		model = list( model.name   = tech.name,
-					 F            = F,
-					 best.params  = as.list(strsplit(resultTable.aux[best.result.idx,3], ",")[[1]]),
-					 mda          = mda.results[best.result.idx],
-					 mddl         = mddl.results[best.result.idx]
-					 )
-
-		save(model, file=paste(modelFolder, '/', tech.name, '_', gsub("[^\\d]+", "", s, perl=TRUE) ,'.RData',sep=''))
-		resultTable.aux
-	}
-
-
-	resultTable = data.frame(resultTable, stringsAsFactors = FALSE)
-	colnames(resultTable) = c("id", "model", "model.params", "series", "mddl", "mda")
-	resultTable = resultTable[order(resultTable$series, as.numeric(resultTable$id)),]
-	resultTable$id = 1:nrow(resultTable)
-
-	return(resultTable)
+exec <- function(series, F, param){
+    result = c()
+    tryCatch({
+        result = F(series, param)
+    }, warning = function(w){
+    }, error = function(e){
+    }, finally = {
+    })
+    if(is.null(result)) result = rep(0, length(series))
+    result
 }
+        
+foreachParam <- function(series, F, hyperparameters){
+    t(apply(hyperparameters, 1, function(x) exec(series, F, x)))
+}
+          
+evaluateResult <- function(obs, resultSeries, m, d){
+    resultTable = matrix(ncol=3,nrow=nrow(resultSeries))
+    resultTable[,1:2] = apply(resultSeries, 
+                              1, 
+                              function(pred) c(TSDecomposition::mddl(obs, pred, plot=FALSE), mda(obs, pred, m, d)))
+    standMddl = (resultTable[,1] - mean(resultTable[,1]))/(sd(resultTable[,1]))
+    standMda  = (resultTable[,2] - mean(resultTable[,2]))/(sd(resultTable[,2]))
+    resultTable[,3] = sqrt(standMddl^2 + standMda^2)
+    colnames(resultTable) = c('MDDL', 'MDA', 'Dist')
+    resultTable
+}
+    
+gridSearch <- function(F, hyperparameters, seriesList, modelFolder, techName, cores = 1, verbose = FALSE){
 
+    registerDoMC(cores)
+
+    resultTable = foreach(i=1:length(seriesList), .combine='rbind') %dopar% {
+        #load series object from RData file
+        seriesObj = seriesList[[i]]
+        m = unlist(seriesObj$det.embDim)
+        d = unlist(seriesObj$det.sepDim)
+        
+        #Cross-validation out-of-time
+        l = ceiling(0.7*seriesObj$size)
+        trainIdx = 1:(l-1)
+        testIdx  = l:seriesObj$size
+    
+        #Grid Search for better hyperparameters
+        if(verbose) cat(paste('ts:',i,'- hyperparameters tunning: begin\n'))
+        startTime = Sys.time()
+        resultSeries   = foreachParam(seriesObj$series[trainIdx], F, hyperparameters)
+        endTime = Sys.time()
+        if(verbose) cat(paste('ts:',i,'- hyperparameters tunning: done - ', endTime - startTime,'\n'))
+        
+        if(verbose) cat(paste('ts:',i,'- evaluation: begin','\n'))
+        startTime = Sys.time()
+        validTestIdx   = which(rowSums(resultSeries) > 0)
+        resultTableAux = evaluateResult(seriesObj$det.series[trainIdx], resultSeries[validTestIdx,], m, d)
+        bestParamIdx   = which.min(resultTableAux[,3])
+        resultTableAux = cbind( rep(techName,nrow(resultTableAux)),
+                                apply(hyperparameters[validTestIdx], 1, function(x) paste(x, collapse=",")),
+                                resultTableAux
+                              )
+        endTime = Sys.time()
+        if(verbose) cat(paste('ts:',i,'- evaluation: done - ', endTime - startTime,'\n'))
+                          
+        if(verbose) cat(paste('ts:',i,'- test series evaluation: begin','\n'))
+        startTime = Sys.time()
+        #run best model with test data and evaluate it
+        bestP = hyperparameters[validTestIdx[bestParamIdx],]
+        pred  = F(seriesObj$series[testIdx], bestP)
+        obs   = seriesObj$det.series[testIdx]
+        mddlR = mddl(obs, pred)
+        mdaR  = mda (obs, pred, seriesObj$det.embDim, seriesObj$det.sepDim)
+        
+        #save model result into model folder
+        model = list( model.name = tech.name, F = F, best.param = bestP, mda = mdaR, mddl = mddlR )
+        save(model, file=paste(modelFolder, '/', techName, '_', gsub("[^\\d]+", "", i, perl=TRUE) ,'.RData',sep=''))
+        endTime = Sys.time()
+        if(verbose) cat(paste('ts:',i,'- test series evaluation: done - ', endTime - startTime,'\n'))
+                          
+        resultTableAux
+    }
+
+    return(resultTable)
+}
