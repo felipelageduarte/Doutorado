56101b3a092b7607f1829e9aa16a91be4b844282
Felipe Duarte <felipelageduarte@gmail.com>
2017-05-27 23:16:07 -0300

    forcedecCorrection

--- 2-emdmi.r
+++ 2-emdmi.r
@@ -16,29 +16,24 @@ calcPhase <- function(x){
   return(r)
 }
 
-emd.fixed <- function(series, tt){
-  r.emd  = EMD::emd(series, tt, boundary = 'wave')
-  r.emd$imf[which(abs(r.emd$imf) < 10^-16)] = 0
-  r.emd$imf = r.emd$imf[,which(colSums(r.emd$imf) != 0)]
-  r.emd$imf = data.frame(r.emd$imf)
-  r.emd$nimf = ncol(r.emd$imf)
-  r.emd$residue[which(r.emd$residue < (10^-16))] = 0
-  r.emd
-}
-
 emdmiDec <- function(series, par){
-  #r.emd  = EMD::emd(series, 1:length(series), boundary = 'wave')
-  r.emd  = emd.fixed(series, 1:length(series))
-  phases = apply(r.emd$imf, 2, function(x){calcPhase(x)})
-  phases = cbind(phases, calcPhase(r.emd$residue))
-  n.size = r.emd$nimf
+  r.emd  = EMD::emd(series, 1:length(series), boundary = 'wave')
+  phases = t(apply(r.emd$imf, 2, function(x){calcPhase(x)}))
+  n.size = r.emd$nimf - 1
+
   # l = 1
   idx = c(1)
   if(n.size > 1){
-    mi = mapply(function(x,y) FNN::mutinfo(x,y),
-                as.data.frame(phases[,1:n.size]),
-                as.data.frame(phases[,2:(n.size+1)]))
+    mi = c()
+    for(i in 1:n.size){
+      mi = c(mi, c3net::makemim(phases[i:(i+1),])[1,2])
+    }
     idx = which(mi >= mean(mi))
+
+    # mi = mapply(function(x,y) c3net::makemim(x,y),
+    #             as.data.frame(phases[,1:n.size]),
+    #             as.data.frame(phases[,2:(n.size+1)]))
+
     # if(length(mi) > 1){
     #
     #   t = mean(mi)
---
+++ 2-forcedecCorrection.r
@@ -0,0 +1,77 @@
+
+source('utils.r')
+
+cores = detectCores(all.tests = FALSE, logical = TRUE)
+
+dataFolder = 'data'
+modelFolder  = 'model'
+resultFolder = 'testResult'
+
+seriesList = loadSeriesFile(dataFolder)
+
+s.emb = tseriesChaos::embedd(series, m, d)
+
+dimSepMean <- function(s.emb, m, d){
+  n = nrow(s.emb) + (m-1)*d
+  idx = matrix(1:n, ncol=m, nrow=n)
+  for(i in 2:m) idx[,i] = idx[,i] - (i-1)*d
+  idx[which(idx < 0 | idx > nrow(s.emb))] = 0
+  for(i in 1:nrow(idx)){
+    s.emb[cbind(idx[i,], 1:m)] = mean(s.emb[cbind(idx[i,], 1:m)])
+  }
+  s.emb
+}
+
+nn.similarity <- function(neigh.pos.i){
+  neigh.dist     = as.matrix(dist(neigh.pos.i))[1,]
+  similarity     = (1 - (neigh.dist/(max(neigh.dist)+10^-12)))
+  similarity[-1] = similarity[-1]/sum(similarity[-1])
+  return(similarity)
+}
+
+forceDecCorrection   <- function(series, par){
+  k        = as.numeric(unlist(par[1]))
+  num.it   = as.numeric(unlist(par[2]))
+  epsilon  = as.numeric(unlist(par[3]))
+  delta    = as.numeric(unlist(par[4]))
+  embedded = as.numeric(unlist(par[5]))
+  delay    = as.numeric(unlist(par[6]))
+
+  s.emb = tseriesChaos::embedd(series, m=embedded, d=delay)
+  nn = get.knn(s.emb, k=k)$nn.index #search for k-nearest neighbor
+  nn = cbind(1:nrow(nn), nn) #place itself as a neighbor
+
+  for(it in 1:num.it){
+    neigh.pos  = lapply(split(nn, 1:nrow(nn)), function(x){s.emb[x,]})
+    norm.simil = lapply(neigh.pos, function(x){nn.similarity(x)})
+    pos = t(mapply(
+      function(x,y, delta){
+        ((delta * x[1,]) + ((1-delta)  * colSums(x[-1,]*y[-1])))
+      },
+      neigh.pos,
+      norm.simil,
+      MoreArgs = list(delta=delta)
+    ))
+
+    pos = dimSepMean(pos)
+    d = s.emb - pos
+    disp = mean(sqrt(diag(d%*%t(d))))
+
+    if(disp <= epsilon) break;
+    s.emb = pos
+  }
+
+  return(toTimeSpace(s.emb, embedded, delay))
+}
+
+params = expand.grid(
+  k = c(2:10,25,50),
+  num.it = 30,
+  epsilon = seq(0.001, 0.01,by=0.001),
+  delta = c(0, 0.25, 0.5, 0.75, 1)
+)
+
+resultTable = gridSearch(forceDecCorrection, params, seriesList, modelFolder, 'forceDecCorrection', cores)
+
+write.csv(resultTable, file=paste(resultFolder,'/forceDecCorrection.csv', sep=''), row.names=FALSE)
+
